fec3dfc0a75136c7385d452fe09e6cabbf05c745
Max Shytikov <mshytikov@gmail.com>
2013-03-04 20:49:08 +0200

    added main

--- project.clj
+++ project.clj
@@ -2,5 +2,7 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.5.0-alpha5"]
                  [aleph "0.3.0-beta7"]
-                 [ring/ring-core "1.1.8"]])
+                 [org.clojure/tools.cli "0.2.2"]
+                 [ring/ring-core "1.1.8"]]
+            :main clj-stream-sh.core)
 
--- src/clj_stream_sh/core.clj
+++ src/clj_stream_sh/core.clj
@@ -1,7 +1,11 @@
 (ns clj-stream-sh.core
   (:import (java.io BufferedReader InputStreamReader))
-  (:use lamina.core aleph.http)
-  (:require [ring.middleware.params :as ring]))
+  (:use
+    lamina.core
+    aleph.http
+    [clojure.tools.cli :only [cli]])
+  (:require [ring.middleware.params :as ring])
+  (:gen-class))
 
 
 (defn cmd [p]
@@ -17,7 +21,7 @@
   (future
     (dorun
       (map #(enqueue ch (str % "\n"))
-        (cmdout (cmd command))))
+           (cmdout (cmd command))))
     (close ch)))
 
 (defn handler [request]
@@ -28,5 +32,19 @@
      :headers {"content-type" "text/plain"}
      :body stream}))
 
-(start-http-server (wrap-ring-handler (ring/wrap-params handler)) {:port 8080})
 
+
+
+
+(defn start [options]
+  (start-http-server (wrap-ring-handler (ring/wrap-params handler)) options))
+
+
+
+(defn -main [& args]
+  (let [[options args banner]
+        (cli args
+             ["-p" "--port" "The port to listen on" :default 8080 :parse-fn #(Integer. %)]
+             ["-h" "--host" "The hostname" :default "localhost"])]
+
+    (start options)))
