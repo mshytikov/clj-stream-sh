221888eebbdd0fd0d65a4dea2b12526ba82d29c8
Max Shytikov <mshytikov@gmail.com>
2013-03-06 19:06:46 +0200

    added url

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject clj-stream-sh "0.0.1-SNAPSHOT"
   :description "HTTP streaming for shell command output."
+  :url "https://github.com/mshytikov/clj-stream-sh"
   :dependencies [[org.clojure/clojure "1.5.0-alpha5"]
                  [aleph "0.3.0-beta14"]
                  [org.clojure/tools.cli "0.2.2"]
