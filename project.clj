8ea1f711d946754163dcb9b911770b8c814e2ffb
Max Shytikov <mshytikov@gmail.com>
2013-03-06 18:42:46 +0200

    added brouser support

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject clj-stream-sh "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.5.0-alpha5"]
-                 [aleph "0.3.0-beta7"]
+                 [aleph "0.3.0-beta14-cms"]
                  [org.clojure/tools.cli "0.2.2"]
                  [ring/ring-core "1.1.8"]]
+            :resources-path "resources"
             :main clj-stream-sh.core)
 
--- src/clj_stream_sh/core.clj
+++ src/clj_stream_sh/core.clj
@@ -1,10 +1,13 @@
 (ns clj-stream-sh.core
   (:import (java.io BufferedReader InputStreamReader))
   (:use
+    clojure.java.io
     lamina.core
     aleph.http
     [clojure.tools.cli :only [cli]])
-  (:require [ring.middleware.params :as ring])
+  (:require 
+    [ring.middleware.params :as ring]
+    [clojure.java.io :as io])
   (:gen-class))
 
 
@@ -22,23 +25,33 @@
     (dorun
       (map #(enqueue ch (str % "\n"))
            (cmdout (cmd command))))
-    (close ch)))
+    (close ch))
+  {:status 200
+   :headers {"content-type" "text/plain"}
+   :body ch})
+
+(defn stream-html [ch filename]
+  (let [html (io/resource filename)]
+    (future
+      (with-open [rdr (io/reader html)]
+        (doseq [line (line-seq rdr)]
+          (enqueue ch (str line "\n"))))
+      ;(enqueue ch html)
+      (close ch))
+    {:status 200
+     :headers {"content-type" "text/html"}
+     :body ch}))
 
 (defn handler [request]
   (let [stream (channel)
-        command (get-in request [:query-params "cmd"] "echo 'Please provide command'")]
-    (stream-sh-output stream command)
-    {:status 200
-     :headers {"content-type" "text/plain"}
-     :body stream}))
-
-
-
+        command (get-in request [:query-params "cmd"])]
+    (if command
+      (stream-sh-output stream command)
+      (stream-html stream "index.html"))))
 
 
 (defn start [options]
   (start-http-server (wrap-ring-handler (ring/wrap-params handler)) options))
-
 
 
 (defn -main [& args]
