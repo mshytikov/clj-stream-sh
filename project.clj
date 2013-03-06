d7f6680ee6eb84d6358d2c70dafc43ed7c92ff1f
Max Shytikov <mshytikov@gmail.com>
2013-03-06 19:04:31 +0200

    fix description

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clj-stream-sh "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject clj-stream-sh "0.0.1-SNAPSHOT"
+  :description "HTTP streaming for shell command output."
   :dependencies [[org.clojure/clojure "1.5.0-alpha5"]
-                 [aleph "0.3.0-beta14-cms"]
+                 [aleph "0.3.0-beta14"]
                  [org.clojure/tools.cli "0.2.2"]
                  [ring/ring-core "1.1.8"]]
             :resources-path "resources"
